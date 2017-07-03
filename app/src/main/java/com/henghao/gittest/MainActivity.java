6f24b532c45c2760c4c31db10e8334f8b8f7e8e8
superzhangbao <15056006309@163.com>
2017-07-03 13:11:09 +0800

    dev_zb:1

--- app/src/main/java/com/henghao/gittest/MainActivity.java
+++ app/src/main/java/com/henghao/gittest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG,"dev:1");
+        Log.i(TAG,"dev_zb:1");
     }
 }
